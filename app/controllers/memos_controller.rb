76f37c14cc2f119f0339e5c025c9a2d4ea12e11c
o4410o <itukusima1025@gmail.com>
2020-10-16 16:09:58 +0900

    メモ機能の実装

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,27 @@
 class MemosController < ApplicationController
+
+  def index
+    @memos = Memo.all
+  end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    @memo = Memo.create(memo_params)
+    
+    if @memo.valid?
+      @memo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def memo_params
+    params.require(:memo).permit(:item, :text)
+  end
 end
