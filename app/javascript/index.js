c97145eea84da648ffbf7269d045bbe00c99e9d7
o4410o <itukusima1025@gmail.com>
2020-10-20 14:25:27 +0900

    自力での実装限界

--- app/javascript/index.js
+++ app/javascript/index.js
@@ -3,12 +3,25 @@ window.addEventListener('load', function(){
 
     for(let i = 0; i < memos.length; i++){
       memos[i].addEventListener('click', function(){
-        memos[i].setAttribute("style", "background-color:#808080;")
-        
-        const element = document.querySelector('section')
-        memos[i].addEventListener('click', function(){
-          element.remove()
-        })
-    })
+        const result = memos[i].hasAttribute('style')
+        if(result == false){
+          memos[i].setAttribute("style", "background-color:#808080;")
+        }
+        // const element = document.querySelector('section')
+        // memos[i].addEventListener('click', function(){
+        //   memos[i].remove()
+        // })
+      })
     }
+    
+      for(let num = 0; num < memos.length; num++){
+        memos[num].addEventListener('click', function(){
+          const result = memos[num].hasAttribute('style')
+          if(result == true){
+              memos[num].remove()
+          
+          }
+          })
+        }
+
 })
